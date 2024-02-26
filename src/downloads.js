7e41e67a2bd92327d7ac8ba973586d3e752cc7aa
sebdie10 <diegoelgaray@gmail.com>
2024-02-25 23:22:35 -0300

    agregar archivo

---
+++ src/downloads.js
@@ -0,0 +1,22 @@
+function descargarRAR() {
+    // Ruta del archivo RAR
+    var rutaArchivoRAR = "/descargar.zip";
+  
+    // Crear un enlace invisible
+    var enlace = document.createElement("a");
+  
+    // Establecer la URL del enlace como la ruta del archivo
+    enlace.href = rutaArchivoRAR;
+  
+    // Establecer el atributo 'download' con el nombre del archivo
+    enlace.download = "extension-tmo.rar";
+  
+    // Agregar el enlace al DOM
+    document.body.appendChild(enlace);
+  
+    // Simular un clic en el enlace para iniciar la descarga
+    enlace.click();
+  
+    // Eliminar el enlace del DOM después de la descarga
+    document.body.removeChild(enlace);
+  }
